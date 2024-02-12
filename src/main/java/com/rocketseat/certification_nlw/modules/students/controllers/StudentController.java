3f768e35e4ec2f2c66d6f5312cb4d515be7cdfed
devnmello <nicolasnns004@gmail.com>
2024-02-12 02:24:23 -0300

    feat: upload use case

--- src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
@@ -1,21 +1,29 @@
 package com.rocketseat.certification_nlw.modules.students.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.useCases.VerifyHasCertificationUseCase;
 
 @RestController
 @RequestMapping("/students")
 
 public class StudentController {
     
+    // Precisa utilizar o USE CASE
+    @Autowired // 'Injeção' de dependencias
+    private VerifyHasCertificationUseCase verifyHasCertificationUseCase;
+
     @PostMapping("/verifyIfHasCertification")
     public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertificationDTO) {
-
-        System.out.println(verifyHasCertificationDTO);
+        var result = this.verifyHasCertificationUseCase.execute(verifyHasCertificationDTO);
+        if(result == true) {
+            return "Usuário já fez a prova";
+        }
         return "Usuário pode fazer a prova";
     }    
 }
