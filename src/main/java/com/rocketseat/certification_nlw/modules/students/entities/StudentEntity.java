a888ac47e89688fd03fcdf846bcae07acc3ca301
devnmello <nicolasnns004@gmail.com>
2024-02-14 02:08:53 -0300

    fix: config

--- src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
@@ -1,8 +1,13 @@
 package com.rocketseat.certification_nlw.modules.students.entities;
 
-import java.util.List;
+//import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +15,17 @@ import lombok.NoArgsConstructor;
 @Data // Adiciona getters/setters automaticamente utilizando Lombok
 @AllArgsConstructor // Cria um construtor com todos os atributos
 @NoArgsConstructor // Cria um construtor vazio
+@Entity(name = "students")
 
 public class StudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id; 
+    
+    @Column(unique = true, nullable = false)
     private String email;
-    private List<CertificationStudentEntity> CertificationStudentEntity;
+
+    //private List<CertificationStudentEntity> CertificationStudentEntity;
     
 }
